2144e3b0e9e6c05bbd8a309f9f27bfeadff99bfd
djafarxyxy <farhanch12344@gmail.com>
2026-09-23 16:11:13 +0700

    q

--- 3.py
+++ 3.py
@@ -1,10 +1,10 @@
 n = int(input("Masukan berapa banyak nilai fibonacci: "))
 
 a, b = 0, 1
-count = 0
+
 
 print("Seri Fibonacci:")
-while count < n:
+while a <= n:
     print(a, end=" ")
     a, b = b, a + b
-    count += 1
+  
